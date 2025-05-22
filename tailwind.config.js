ff8574a3b51cfc16265305691653a1c4556e9765
Andy Nugrahan Putra <andybima6@gmail.com>
2025-05-22 22:53:48 +0700

    feat: add AnimatedElement component for enhanced animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",  
-    "./pages/**/*.{js,ts,jsx,tsx}", 
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["animate-fade-in"],
   theme: {
     extend: {
       colors: {
-        limeCustom: '#79A900',
+        limeCustom: "#79A900",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
